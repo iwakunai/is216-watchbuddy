5dfd3379acab7713b77205f1120310e57c708ac1
koayyiting <koayyiting@gmail.com>
2025-10-13 23:31:11 +0800

    trending movies in home page

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- flex-shrink-0 : prevent flex item from shrinking -->
+    <div class="movie_card flex-shrink-0 w-[160px]">
+        <a href="#">
+            <img class="trending_movie_img h-[auto] rounded-md object-cover" :src="movie.img"/>
+        </a>
+        <div class="text-left mt-2">
+            <p class="text-lg font-semibold">{{ movie.title }}</p>
+            <p class="font-light">{{ movie.releaseDate }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    movie: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+</style>
