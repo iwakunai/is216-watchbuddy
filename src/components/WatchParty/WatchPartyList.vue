ba31aeec34ba274b744142ed0f6d499bc96db581
koayyiting <koayyiting@gmail.com>
2025-11-05 22:48:50 +0800

    small change

--- src/components/WatchParty/WatchPartyList.vue
+++ src/components/WatchParty/WatchPartyList.vue
@@ -2,7 +2,7 @@
   <!-- WATCH LIST -->
   <div class="mb-8 px-10">
     <!-- WATCH PARTY ROOM CARDS -->
-    <div class="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+    <div class="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
       <RoomCard v-for="room in slicedRooms" :key="room.roomid" :room="room" />
     </div>
 
